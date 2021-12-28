b5dc45cbc121882992833ed0dfd178cf56f9f82c
Araf Rahman <arahman.usa500@gmail.com>
2021-12-27 19:27:07 -0500

    changes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
 import studentRoutes from './routes/student.js'; 
 
 const app = express()
@@ -19,7 +19,7 @@ app.use('/students', studentRoutes)
 
 const CONNECTION_URL = 'mongodb+srv://vcupikes:vcupikes@cluster0.63d7s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-const PORT = process.env.PORT|| 9000;
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
